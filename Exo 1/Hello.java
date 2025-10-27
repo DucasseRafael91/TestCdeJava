4e8bb2b661bc7875345776e4a03420d63d0ae713
Rafael Ducasse <rafael.ducasse@fms-ea.com>
2025-10-27 13:13:07 +0100

    first commit

---
+++ Exo 1/Hello.java
@@ -0,0 +1,15 @@
+package test;
+
+public class Hello {
+	
+    public static void main(String[] args) {
+    	
+        if (args.length > 0) {
+            System.out.println("Bonjour " + args[0]);
+        } 
+        else {
+            System.out.println("Bonjour");
+        }
+    }
+}
+
