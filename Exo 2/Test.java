6e6a51cf8be534e5dcb59807a26d312dc4e4ecda
Rafael Ducasse <rafael.ducasse@fms-ea.com>
2025-10-27 13:31:46 +0100

    Question 2.3 de l'exo 2

--- Exo 2/Test.java
+++ Exo 2/Test.java
@@ -1,25 +1,30 @@
 import java.util.Scanner;
 
 public class Test {
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Entrez un nombre entier : ");
-        int nombre = scanner.nextInt();
 
-        if (nombre > 0) {
-            System.out.println("Le nombre est positif.");
-        } else if (nombre < 0) {
-            System.out.println("Le nombre est négatif.");
-        } else {
-            System.out.println("Le nombre est nul.");
-        }
+        // Vérifie si la saisie est bien un entier
+        if (scanner.hasNextInt()) {
+            int nombre = scanner.nextInt();
 
-	if (nombre % 2 == 0) {
-            System.out.println("Le nombre est pair");
+            if (nombre > 0) {
+                System.out.println("Le nombre est positif.");
+            } else if (nombre < 0) {
+                System.out.println("Le nombre est négatif.");
+            } else {
+                System.out.println("Le nombre est nul.");
+            }
+
+            if (nombre % 2 == 0) {
+                System.out.println("Le nombre est pair.");
+            } else {
+                System.out.println("Le nombre est impair.");
+            }
         } else {
-            System.out.println("Le nombre est impair");
+            System.out.println("La valeur n'est pas un entier");
         }
 
         scanner.close();
