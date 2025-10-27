c91b8469f7f47cadaf997eeb133cba0c99960a47
Rafael Ducasse <rafael.ducasse@fms-ea.com>
2025-10-27 13:18:32 +0100

    Question 1.2 de l'exo 1

--- Exo 1/Hello.java
+++ Exo 1/Hello.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Bonjour et bienvenue dans mon programme Java");
+        Scanner scanner = new Scanner(System.in); // pour lire l’entrée utilisateur
+
+        System.out.println("Bonjour et bienvenue dans mon programme Java !");
+        System.out.print("Quel est votre nom ? ");
+
+        String nom = scanner.nextLine(); // lit la ligne saisie par l’utilisateur
+
+        System.out.println("Salut " + nom + " !");
+        
+        scanner.close(); // bonne pratique : fermer le scanner
     }
 }
