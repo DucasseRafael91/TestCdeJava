90293445624b41d3d019105a4485c697c4fe9130
Rafael Ducasse <rafael.ducasse@fms-ea.com>
2025-10-27 13:16:10 +0100

    Question 1.1 de l'exo 1

--- Exo 1/Hello.java
+++ Exo 1/Hello.java
@@ -1,15 +1,5 @@
-package test;
-
 public class Hello {
-	
     public static void main(String[] args) {
-    	
-        if (args.length > 0) {
-            System.out.println("Bonjour " + args[0]);
-        } 
-        else {
-            System.out.println("Bonjour");
-        }
+        System.out.println("Bonjour et bienvenue dans mon programme Java");
     }
 }
-
