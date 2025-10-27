c15779466ab5a856e31d3b7bda8e3ce29c8b9050
Rafael Ducasse <rafael.ducasse@fms-ea.com>
2025-10-27 13:43:36 +0100

    Question 2.4 de l'exo 2

--- Exo 2/Test.java
+++ Exo 2/Test.java
@@ -2,14 +2,31 @@ import java.util.Scanner;
 
 public class Test {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        int nombre = 0;
+        boolean saisieValide = false;
 
-        System.out.print("Entrez un nombre entier : ");
+        if (args.length > 0) {
+            try {
+                nombre = Integer.parseInt(args[0]);
+                saisieValide = true;
+            } catch (NumberFormatException e) {
+                System.out.println("Erreur : l'argument fourni n'est pas un entier !");
+            }
+        }
 
-        // Vérifie si la saisie est bien un entier
-        if (scanner.hasNextInt()) {
-            int nombre = scanner.nextInt();
+        if (!saisieValide) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Entrez un nombre entier : ");
+            if (scanner.hasNextInt()) {
+                nombre = scanner.nextInt();
+                saisieValide = true;
+            } else {
+                System.out.println("Erreur : la valeur saisie n'est pas un entier !");
+            }
+            scanner.close();
+        }
 
+        if (saisieValide) {
             if (nombre > 0) {
                 System.out.println("Le nombre est positif.");
             } else if (nombre < 0) {
@@ -23,10 +40,6 @@ public class Test {
             } else {
                 System.out.println("Le nombre est impair.");
             }
-        } else {
-            System.out.println("La valeur n'est pas un entier");
         }
-
-        scanner.close();
     }
 }
